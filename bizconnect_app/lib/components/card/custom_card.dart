6d253ea6808e8a94536a7ff3c1d04b5910af848a
wiratr <wirateds@gmail.com>
2024-07-14 09:19:56 +0700

    change home

--- bizconnect_app/lib/components/card/custom_card.dart
+++ bizconnect_app/lib/components/card/custom_card.dart
@@ -1,272 +1,72 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+  final VoidCallback onTap;
+
+  const CustomCard({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.onTap,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 179,
-          height: 425,
-          decoration: ShapeDecoration(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(width: 0.48, color: Color(0xFFEEEEEE)),
-              borderRadius: BorderRadius.circular(8.70),
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 179,
-                height: 253.58,
-                clipBehavior: Clip.antiAlias,
-                decoration: ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.70),
-                  ),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 189.36,
-                        height: 96.61,
-                        decoration: BoxDecoration(color: Color(0xFFD9D9D9)),
-                      ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      child: Container(
-                        width: 189.33,
-                        height: 268.22,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage("https://picsum.photos/189/268"),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      top: -19.80,
-                      child: Container(
-                        width: 179,
-                        height: 323.94,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage("https://picsum.photos/179/324"),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 126,
-                      top: 13,
-                      child: Container(
-                        width: 40,
-                        height: 40,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              child: Container(
-                                width: 40,
-                                height: 40,
-                                decoration: ShapeDecoration(
-                                  shape: OvalBorder(
-                                    side: BorderSide(width: 1, color: Colors.white),
-                                  ),
-                                  shadows: [
-                                    BoxShadow(
-                                      color: Color(0x1413B4FF),
-                                      blurRadius: 5.13,
-                                      offset: Offset(0, 1.03),
-                                      spreadRadius: 0,
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              left: 9,
-                              top: 9,
-                              child: Container(
-                                width: 21.72,
-                                height: 21.72,
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      width: 21.72,
-                                      height: 21.72,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        children: [
-                                          Container(
-                                            width: 21.72,
-                                            height: 21.72,
-                                            child: Stack(children: []),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 106.36,
-                padding: const EdgeInsets.symmetric(horizontal: 8.99, vertical: 5.26),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  width: 159,
-                                  child: Text(
-                                    '20 Mar, 2024 / 16:00-21:00',
-                                    style: TextStyle(
-                                      color: Color(0xFF56BED6),
-                                      fontSize: 12,
-                                      fontFamily: 'FC Iconic',
-                                      height: 1.5,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 5.80),
-                    Container(
-                      width: 170,
-                      height: 62,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 169,
-                            height: 62,
-                            child: Text(
-                              '4th Global Meeting on Diabetes and Endocrinology',
-                              style: TextStyle(
-                                color: Color(0xFF122D58),
-                                fontSize: 14,
-                                fontFamily: 'FC Iconic',
-                                height: 1.5,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 5.80),
-                    Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 13,
-                            padding: const EdgeInsets.only(bottom: 13.81),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: double.infinity,
-                                  height: 13.19,
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        width: 13.19,
-                                        height: 13.19,
-                                        child: Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          crossAxisAlignment: CrossAxisAlignment.center,
-                                          children: [
-                                            Container(
-                                              width: 13.19,
-                                              height: 13.19,
-                                              child: Stack(children: []),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(width: 2.63),
-                          SizedBox(
-                            width: 131,
-                            child: Text(
-                              'The Imperial Hotel Bangkok',
-                              style: TextStyle(
-                                color: Color(0xFF91A6C4),
-                                fontSize: 12,
-                                fontFamily: 'FC Iconic',
-                                height: 1.5,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
         ),
-      ],
+        elevation: 5,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0),
+              ),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 150.0,
+                  maxWidth: double.infinity,
+                ),
+                child: Image.network(
+                  imageUrl,
+                  width: double.infinity,
+                  height: 150.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                description,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- bizconnect_app/lib/screens/home/home_screen.dart
+++ bizconnect_app/lib/screens/home/home_screen.dart
@@ -159,97 +159,183 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
 Widget _buildHomeContent() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Popular Events',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => PopularViewAllScreen()),
-                    );
-                  },
-                  child: Text(
-                    'view all',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.blue, // Same color as the text
-                    ),
+  return SingleChildScrollView (
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Popular Events',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PopularViewAllScreen()),
+                  );
+                },
+                child: Text(
+                  'view all',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.blue, // Same color as the text
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: CustomCard(), // Left card
-                ),
-                SizedBox(width: 10), // Space between cards
-                Expanded(
-                  child: CustomCard(), // Right card
-                ),
-              ],
-            ),
-            SizedBox(height: 20), // Space between sections
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Special Events',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SpecialViewAllScreen()),
-                    );
+              ),
+            ],
+          ),
+          // create card
+          Row(
+            children: [
+              Flexible(
+                child: CustomCard(
+                  title: 'Card Title 1',
+                  description: 'This is the description for card 1.',
+                  imageUrl: 'https://via.placeholder.com/150',
+                  onTap: () {
+                    // print('Card 1 tapped');
                   },
-                  child: Text(
-                    'view all',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Colors.blue, // Same color as the text
-                    ),
-                  ),
                 ),
-              ],
-            ),
-            // Text(
-            //   'Special Events',
-            //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            // ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: CustomCard(), // Left card
-                ),
-                SizedBox(width: 10), // Space between cards
-                Expanded(
-                  child: CustomCard(), // Right card
-                ),
-              ],
-            ),
-            // Add more rows or other content as needed
-          ],
-        ),
+              ),
+              // Flexible(
+              //   child: CustomCard(
+              //     title: 'Card Title 2',
+              //     description: 'This is the description for card 2.',
+              //     imageUrl: 'https://via.placeholder.com/150',
+              //     onTap: () {
+              //       print('Card 2 tapped');
+              //     },
+              //   ),
+              // ),
+            ],
+          ),
+          // 
+        ],
       ),
-    );
-  }
+    ),
+  );
+
+} 
+
+// Widget _buildHomeContent() {
+//     return SingleChildScrollView(
+//       child: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Text(
+//                   'Popular Events',
+//                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//                 ),
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => PopularViewAllScreen()),
+//                     );
+//                   },
+//                   child: Text(
+//                     'view all',
+//                     style: TextStyle(
+//                       color: Colors.blue,
+//                       decoration: TextDecoration.underline,
+//                       decorationColor: Colors.blue, // Same color as the text
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 CustomCard(
+//                   title: 'Card Title 1',
+//                   description: 'This is the description for card 1.',
+//                   imageUrl: 'https://via.placeholder.com/150',
+//                   onTap: () {
+//                     print('Card 1 tapped');
+//                   },
+//                 ),
+//               ],
+//             ),
+//             // SizedBox(height: 10),
+//             // Row(
+//             //   children: [
+//             //     CustomCard(
+//             //       title: 'Card Title 1',
+//             //       description: 'This is the description for card 1.',
+//             //       imageUrl: 'https://via.placeholder.com/150',
+//             //       onTap: () {
+//             //         print('Card 1 tapped');
+//             //       },
+//             //     ),
+//             //     // Expanded(
+//             //     //   child: CustomCard(), // Left card
+//             //     // ),
+//             //     // SizedBox(width: 10), // Space between cards
+//             //     // Expanded(
+//             //     //   child: CustomCard(), // Right card
+//             //     // ),
+//             //   ],
+//             // ),
+//             // // SizedBox(height: 20), // Space between sections
+//             // Row(
+//             //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             //   children: [
+//             //     Text(
+//             //       'Special Events',
+//             //       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//             //     ),
+//             //     TextButton(
+//             //       onPressed: () {
+//             //         Navigator.push(
+//             //           context,
+//             //           MaterialPageRoute(builder: (context) => SpecialViewAllScreen()),
+//             //         );
+//             //       },
+//             //       child: Text(
+//             //         'view all',
+//             //         style: TextStyle(
+//             //           color: Colors.blue,
+//             //           decoration: TextDecoration.underline,
+//             //           decorationColor: Colors.blue, // Same color as the text
+//             //         ),
+//             //       ),
+//             //     ),
+//             //   ],
+//             // ),
+//             // Text(
+//             //   'Special Events',
+//             //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//             // ),
+//             // SizedBox(height: 10),
+//             // Row(
+//             //   children: [
+//             //     Expanded(
+//             //       child: CustomCard(), // Left card
+//             //     ),
+//             //     SizedBox(width: 10), // Space between cards
+//             //     Expanded(
+//             //       child: CustomCard(), // Right card
+//             //     ),
+//             //   ],
+//             // ),
+//             // Add more rows or other content as needed
+//           ],
+//         ),
+//       ),
+//     );
+//   }
 
   void _onItemTapped(int index) {
     setState(() {
